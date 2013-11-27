4af9077e2137940bad64b2c2188b0c9f8884668c
Jared Fraser <dev@jsf.io>
2013-11-27 16:58:15 +0000

    Properly namespaces classes

--- app/models/cms/revision.rb
+++ app/models/cms/revision.rb
@@ -1,12 +1,14 @@
-class Cms::Revision < ActiveRecord::Base
-  include Cms::Base
-  
-  serialize :data
-  
-  # -- Relationships --------------------------------------------------------
-  belongs_to :record, :polymorphic => true
-  
-  # -- Scopes ---------------------------------------------------------------
-  default_scope -> { order('created_at DESC') }
-  
+module Cms
+  class Revision < ActiveRecord::Base
+    include Cms::Base
+
+    serialize :data
+
+    # -- Relationships --------------------------------------------------------
+    belongs_to :record, :polymorphic => true
+
+    # -- Scopes ---------------------------------------------------------------
+    default_scope -> { order('created_at DESC') }
+
+  end
 end
