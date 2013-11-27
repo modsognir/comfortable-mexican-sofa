a8a7f19c485bfa9beb8356778d0be185aa410ce1
Jared Fraser <dev@jsf.io>
2013-11-27 16:56:55 +0000

    Refactor content controller to accept respond_to blocks

--- app/controllers/cms/content_controller.rb
+++ app/controllers/cms/content_controller.rb
@@ -1,51 +1,59 @@
-class Cms::ContentController < Cms::BaseController
-  
-  # Authentication module must have #authenticate method
-  include ComfortableMexicanSofa.config.public_auth.to_s.constantize
-  
-  before_action :load_fixtures
-  before_action :load_cms_page,
-                :authenticate,
-                :only => :render_html
-  
-  def render_html(status = 200)
-    if @cms_layout = @cms_page.layout
-      app_layout = (@cms_layout.app_layout.blank? || request.xhr?) ? false : @cms_layout.app_layout
-      render :inline => @cms_page.content, :layout => app_layout, :status => status, :content_type => 'text/html'
-    else
-      render :text => I18n.t('cms.content.layout_not_found'), :status => 404
+module Cms
+  class ContentController < Cms::BaseController
+
+    respond_to :json, :html
+
+    # Authentication module must have #authenticate method
+    include ComfortableMexicanSofa.config.public_auth.to_s.constantize
+
+    before_action :load_fixtures
+    before_action :authenticate,
+                  :only => :show
+
+    rescue_from ActiveRecord::RecordNotFound, with: :page_not_found
+
+
+    def show
+      @cms_page = @cms_site.find_published_page!("/#{params[:cms_path]}")
+
+      if @cms_page.is_a?(RedirectedPage)
+        redirect_to @cms_page.target_page
+      else
+        respond_with(@cms_page) do |format|
+          format.html { render_html }
+        end
+      end
     end
-  end
 
-  def render_sitemap
-    render
-  end
+    def render_sitemap
+      render
+    end
 
-  def render_css
-    render :text => @cms_layout.css, :content_type => 'text/css'
-  end
+  protected
 
-  def render_js
-    render :text => @cms_layout.js, :content_type => 'text/javascript'
-  end
+    def render_html(status = nil)
+      if @cms_layout = @cms_page.layout
+        app_layout = (@cms_layout.app_layout.blank? || request.xhr?) ? false : @cms_layout.app_layout
+        render :inline => @cms_page.content, :layout => app_layout, :status => status, :content_type => 'text/html'
+      else
+        render :text => I18n.t('cms.content.layout_not_found'), :status => 404
+      end
+    end
 
-protected
+    def load_fixtures
+      return unless ComfortableMexicanSofa.config.enable_fixtures
+      ComfortableMexicanSofa::Fixture::Importer.new(@cms_site.identifier).import!
+    end
 
-  def load_fixtures
-    return unless ComfortableMexicanSofa.config.enable_fixtures
-    ComfortableMexicanSofa::Fixture::Importer.new(@cms_site.identifier).import!
-  end
-  
-  def load_cms_page
-    @cms_page = @cms_site.pages.published.find_by_full_path!("/#{params[:cms_path]}")
-    return redirect_to(@cms_page.target_page.url) if @cms_page.target_page
-    
-  rescue ActiveRecord::RecordNotFound
-    if @cms_page = @cms_site.pages.published.find_by_full_path('/404')
-      render_html(404)
-    else
+    def page_not_found
+      @cms_page = @cms_site.find_published_page!('/404')
+
+      respond_with @cms_page do |format|
+        format.html { render_html(404) }
+      end
+
+    rescue ActiveRecord::RecordNotFound
       raise ActionController::RoutingError.new("Page Not Found at: \"#{params[:cms_path]}\"")
     end
   end
-
 end
--- app/models/cms/page.rb
+++ app/models/cms/page.rb
@@ -2,16 +2,16 @@
 
 class Cms::Page < ActiveRecord::Base
   include Cms::Base
-  
+
   cms_acts_as_tree :counter_cache => :children_count
   cms_is_categorized
   cms_is_mirrored
   cms_has_revisions_for :blocks_attributes
-  
+
   attr_accessor :tags,
                 :cached_content,
                 :blocks_attributes_changed
-  
+
   # -- Relationships --------------------------------------------------------
   belongs_to :site
   belongs_to :layout
@@ -20,7 +20,7 @@ class Cms::Page < ActiveRecord::Base
   has_many :blocks,
     :autosave   => true,
     :dependent  => :destroy
-  
+
   # -- Callbacks ------------------------------------------------------------
   before_validation :assigns_label,
                     :assign_parent,
@@ -30,9 +30,9 @@ class Cms::Page < ActiveRecord::Base
   before_save       :set_cached_content
   after_save        :sync_child_full_paths!
   after_find        :unescape_slug_and_path
-  
+
   # -- Validations ----------------------------------------------------------
-  validates :site_id, 
+  validates :site_id,
     :presence   => true
   validates :label,
     :presence   => true
@@ -44,11 +44,11 @@ class Cms::Page < ActiveRecord::Base
     :presence   => true
   validate :validate_target_page
   validate :validate_format_of_unescaped_slug
-  
+
   # -- Scopes ---------------------------------------------------------------
   default_scope -> { order('cms_pages.position') }
   scope :published, -> { where(:is_published => true) }
-  
+
   # -- Class Methods --------------------------------------------------------
   # Tree-like structure for pages
   def self.options_for_select(site, page = nil, current_page = nil, depth = 0, exclude_self = true, spacer = '. . ')
@@ -58,21 +58,21 @@ class Cms::Page < ActiveRecord::Base
     current_page.children.each do |child|
       out += options_for_select(site, page, child, depth + 1, exclude_self, spacer)
     end if current_page.children_count.nonzero?
-    return out.compact 
+    return out.compact
   end
-  
+
   # -- Instance Methods -----------------------------------------------------
   # For previewing purposes sometimes we need to have full_path set. This
   # full path take care of the pages and its childs but not of the site path
   def full_path
     self.read_attribute(:full_path) || self.assign_full_path
   end
-  
+
   # Somewhat unique method of identifying a page that is not a full_path
   def identifier
     self.parent_id.blank?? 'index' : self.full_path[1..-1].slugify
   end
-  
+
   # Transforms existing cms_block information into a hash that can be used
   # during form processing. That's the only way to modify cms_blocks.
   def blocks_attributes(was = false)
@@ -83,7 +83,7 @@ class Cms::Page < ActiveRecord::Base
       block_attr
     end
   end
-  
+
   # Array of block hashes in the following format:
   #   [
   #     { :identifier => 'block_1', :content => 'block content' },
@@ -93,25 +93,25 @@ class Cms::Page < ActiveRecord::Base
     block_hashes = block_hashes.values if block_hashes.is_a?(Hash)
     block_hashes.each do |block_hash|
       block_hash.symbolize_keys! unless block_hash.is_a?(HashWithIndifferentAccess)
-      block = 
-        self.blocks.detect{|b| b.identifier == block_hash[:identifier]} || 
+      block =
+        self.blocks.detect{|b| b.identifier == block_hash[:identifier]} ||
         self.blocks.build(:identifier => block_hash[:identifier])
       block.content = block_hash[:content]
       self.blocks_attributes_changed = self.blocks_attributes_changed || block.content_changed?
     end
   end
-  
-  # Processing content will return rendered content and will populate 
+
+  # Processing content will return rendered content and will populate
   # self.cms_tags with instances of CmsTag
   def render
     @tags = [] # resetting
     return '' unless layout
-    
+
     ComfortableMexicanSofa::Tag.process_content(
       self, ComfortableMexicanSofa::Tag.sanitize_irb(layout.merged_content)
     )
   end
-  
+
   # Cached content accessor
   def content
     if (@cached_content = read_attribute(:content)).nil?
@@ -120,50 +120,50 @@ class Cms::Page < ActiveRecord::Base
     end
     @cached_content
   end
-  
+
   def clear_cached_content!
     self.update_column(:content, nil)
   end
-  
+
   # Array of cms_tags for a page. Content generation is called if forced.
   # These also include initialized cms_blocks if present
   def tags(force_reload = false)
     self.render if force_reload
     @tags ||= []
   end
-  
+
   # Full url for a page
   def url
     "//" + "#{self.site.hostname}/#{self.site.path}/#{self.full_path}".squeeze("/")
   end
-  
+
   # Method to collect prevous state of blocks for revisions
   def blocks_attributes_was
     blocks_attributes(true)
   end
-  
+
 protected
-  
+
   def assigns_label
     self.label = self.label.blank?? self.slug.try(:titleize) : self.label
   end
-  
+
   def assign_parent
     return unless site
     self.parent ||= site.pages.root unless self == site.pages.root || site.pages.count == 0
   end
-  
+
   def assign_full_path
     self.full_path = self.parent ? "#{CGI::escape(self.parent.full_path).gsub('%2F', '/')}/#{self.slug}".squeeze('/') : '/'
   end
-  
+
   def assign_position
     return unless self.parent
     return if self.position.to_i > 0
     max = self.parent.children.maximum(:position)
     self.position = max ? max + 1 : 0
   end
-  
+
   def validate_target_page
     return unless self.target_page
     p = self
@@ -171,18 +171,18 @@ protected
       return self.errors.add(:target_page_id, 'Invalid Redirect') if (p = p.target_page) == self
     end
   end
-  
+
   def validate_format_of_unescaped_slug
     return unless slug.present?
     unescaped_slug = CGI::unescape(self.slug)
     errors.add(:slug, :invalid) unless unescaped_slug =~ /^\p{Alnum}[\.\p{Alnum}\p{Mark}_-]*$/i
   end
-  
+
   def set_cached_content
     @cached_content = self.render
     write_attribute(:content, self.cached_content)
   end
-  
+
   # Forcing re-saves for child pages so they can update full_paths
   def sync_child_full_paths!
     return unless full_path_changed?
@@ -202,5 +202,5 @@ protected
     self.slug       = CGI::unescape(self.slug)      unless self.slug.nil?
     self.full_path  = CGI::unescape(self.full_path) unless self.full_path.nil?
   end
-  
+
 end
