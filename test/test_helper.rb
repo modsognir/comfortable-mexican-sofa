088839b45fdc62202be5b976774014b232b15f88
Jared Fraser <dev@jsf.io>
2013-11-28 15:54:07 +0000

    Added Pry for debugging

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,18 +9,20 @@ require 'rails/test_help'
 require 'rails/generators'
 require 'mocha/setup'
 
+require 'pry'
+
 # No need to add cache-busters in test environment
 Paperclip::Attachment.default_options[:use_timestamp] = false
 
 class ActiveSupport::TestCase
   fixtures :all
   include ActionDispatch::TestProcess
-  
+
   def setup
     reset_config
     stub_paperclip
   end
-  
+
   # resetting default configuration
   def reset_config
     ComfortableMexicanSofa.configure do |config|
@@ -31,7 +33,7 @@ class ActiveSupport::TestCase
       config.enable_fixtures      = false
       config.fixtures_path        = File.expand_path('db/cms_fixtures', Rails.root)
       config.revisions_limit      = 25
-      config.locales              = { 
+      config.locales              = {
         'en'    => 'English',
         'es'    => 'Español',
         'pt-BR' => 'Português Brasileiro',
@@ -49,7 +51,7 @@ class ActiveSupport::TestCase
     ComfortableMexicanSofa::HttpAuth.username = 'username'
     ComfortableMexicanSofa::HttpAuth.password = 'password'
   end
-  
+
   # Example usage:
   #   assert_has_errors_on @record, :field_1, :field_2
   def assert_has_errors_on(record, *fields)
@@ -58,7 +60,7 @@ class ActiveSupport::TestCase
     unmatched = fields.flatten - record.errors.keys
     assert unmatched.blank?, "#{record.class} doesn't have errors on '#{unmatched.join(', ')}'"
   end
-  
+
   # Example usage:
   #   assert_exception_raised                                 do ... end
   #   assert_exception_raised ActiveRecord::RecordInvalid     do ... end
@@ -88,13 +90,13 @@ class ActiveSupport::TestCase
   def rendered_content_formatter(string)
     string.gsub(/^[ ]+/, '')
   end
-  
+
   def stub_paperclip
     Cms::Block.any_instance.stubs(:save_attached_files).returns(true)
     Cms::Block.any_instance.stubs(:delete_attached_files).returns(true)
     Paperclip::Attachment.any_instance.stubs(:post_process).returns(true)
   end
-  
+
 end
 
 class ActionController::TestCase
@@ -104,13 +106,13 @@ class ActionController::TestCase
 end
 
 class ActionDispatch::IntegrationTest
-  
+
   def setup
     host! 'test.host'
     reset_config
     stub_paperclip
   end
-  
+
   # Attaching http_auth stuff with request. Example use:
   #   http_auth :get, '/cms-admin/pages'
   def http_auth(method, path, options = {}, username = 'username', password = 'password')
@@ -119,12 +121,12 @@ class ActionDispatch::IntegrationTest
 end
 
 class Rails::Generators::TestCase
-  
+
   destination File.expand_path('../tmp', File.dirname(__FILE__))
-  
+
   setup :prepare_destination,
         :prepare_files
-  
+
   def prepare_files
     config_path = File.join(self.destination_root, 'config')
     routes_path = File.join(config_path, 'routes.rb')
@@ -134,7 +136,7 @@ class Rails::Generators::TestCase
       f.write("Test::Application.routes.draw do\n\nend")
     end
   end
-  
+
   def read_file(filename)
     File.read(
       File.join(
@@ -143,5 +145,5 @@ class Rails::Generators::TestCase
       )
     )
   end
-  
+
 end
